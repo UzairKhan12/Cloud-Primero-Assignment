e0ce980aed2f1026c64dbd3ec52a1d5dae86ad7c
Uzair <uzairkhan141@gmail.com>
2022-04-18 01:15:16 +0500

    dot env added

--- config/constants.js
+++ config/constants.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    mongodb_url: process.env.MONGODB_URL ,
+    mongodb_url: process.env.MONGODB_URL || "mongodb://localhost:27017/assignment",
     server_port: process.env.PORT || 8081,
     base_url: process.env.BASE_URL || 'http://localhost'
 };
